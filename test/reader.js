56fe4328eb420dc9c9eeb4207c97688c921cc1df
daiiz <da.iz.mg@gmail.com>
2018-12-17 19:29:17 +0900

    add reader test

---
+++ test/reader.js
@@ -0,0 +1,32 @@
+const {assert} = require('chai')
+const fs = require('fs')
+const {parsePngFormat} = require('../lib/')
+
+describe('parseImages', function () {
+  it('detect width, height and dpi from Retina screenshot png image', function () {
+    // Screenshot with Retina display taken by macOS screenshot app
+    const buf = fs.readFileSync('./test/images/dpi-144.png')
+    const {width, height, dpi} = parsePngFormat(buf)
+    assert.equal(width, 170)
+    assert.equal(height, 100)
+    assert.equal(dpi, 144)
+  })
+
+  it('detect width and height, return undefined for dpi', function () {
+    // Screenshot with non-Retina display taken by macOS screenshot app
+    const buf = fs.readFileSync('./test/images/dpi-empty.png')
+    const {width, height, dpi} = parsePngFormat(buf)
+    assert.equal(width, 85)
+    assert.equal(height, 50)
+    assert.isUndefined(dpi)
+  })
+
+  it('return undefined for width, height and dpi', function () {
+    // Not PNG image
+    const buf = fs.readFileSync('./test/images/not-png.gif')
+    const {width, height, dpi} = parsePngFormat(buf)
+    assert.isUndefined(width)
+    assert.isUndefined(height)
+    assert.isUndefined(dpi)
+  })
+})
