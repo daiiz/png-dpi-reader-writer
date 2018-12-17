72fd2ad96e2a8a5e8e7e2e423c254ec0ca97258e
daiiz <da.iz.mg@gmail.com>
2018-12-17 18:41:53 +0900

    utils -> share

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import {writePngDpi} from './writer'
-import {readPngDpi} from './reader'
-const dataURIScheme = 'data:image/png;base64,'
-
-const convertToDataURI = byteArray => {
-  return dataURIScheme + btoa(byteArray.reduce((data, byte) => {
-    return data + String.fromCharCode(byte)
-  }, ''))
-}
-
-const convertToByteArray = dataURI => {
-  if (!dataURI.startsWith(dataURIScheme)) return []
-  const rawStr = atob(dataURI.split(dataURIScheme)[1])
-  const array = new Uint8Array(new ArrayBuffer(rawStr.length))
-  for (let i = 0; i < rawStr.length; i++) {
-    array[i] = rawStr.charCodeAt(i)
-  }
-  return array
-}
-
-const writeChunkPhys = writePngDpi
+import {parsePngFormat} from './reader'
+import {convertToDataURI, convertToByteArray} from './share'
 
 export {
-  readPngDpi, writePngDpi, convertToDataURI, convertToByteArray,
-  writeChunkPhys
+  parsePngFormat, writePngDpi,
+  convertToDataURI, convertToByteArray
 }
--- src/share.js
+++ src/share.js
@@ -9,9 +9,9 @@ export function bytes (num, bytes) {
 }
 
 export function isPng (byteArray, ptr) {
-  const pngSignature = /^89 50 4E 47 0D 0A 1A 0A$/i
+  const pngSignature = '89 50 4E 47 0D 0A 1A 0A'
   const signature = readBytes(byteArray, ptr, 8).map(v => toHex(v, 2))
-  return signature.join(' ').match(pngSignature)
+  return signature.join(' ').toUpperCase() === pngSignature
 }
 
 export function readBytes (byteArray, ptr, bytes) {
@@ -25,13 +25,29 @@ export function readIHDR (byteArray, ptr) {
   // https://tools.ietf.org/html/rfc2083#page-15
   // Length, ChunkType
   ptr.pos += (4 + 4)
-  // Width
   let width = readBytes(byteArray, ptr, 4).map(v => toBin(v, 8))
   width = parseInt(width.join(''), 2)
-  // Height
   let height = readBytes(byteArray, ptr, 4).map(v => toBin(v, 8))
   height = parseInt(height.join(''), 2)
   // Bit depth, Color type, Compression method, Filter method, nterlace method, CRC
   ptr.pos += (1 + 1 + 1 + 1 + 1 + 4)
   return {width, height}
+}
+
+const dataURIScheme = 'data:image/png;base64,'
+
+export function convertToDataURI (byteArray) {
+  return dataURIScheme + btoa(byteArray.reduce((data, byte) => {
+    return data + String.fromCharCode(byte)
+  }, ''))
+}
+
+export function convertToByteArray (dataURI) {
+  if (!dataURI.startsWith(dataURIScheme)) return []
+  const rawStr = atob(dataURI.split(dataURIScheme)[1])
+  const array = new Uint8Array(new ArrayBuffer(rawStr.length))
+  for (let i = 0; i < rawStr.length; i++) {
+    array[i] = rawStr.charCodeAt(i)
+  }
+  return array
 }
