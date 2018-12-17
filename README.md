d28864a5c0de3234f297fc1b31e2a7d8483497cc
daiiz <da.iz.mg@gmail.com>
2018-12-17 18:58:10 +0900

    Updates

---
+++ README.md
@@ -0,0 +1,8 @@
+# png-chunk-pHYs
+- https://daiiz.github.io/png-chunk-pHYs/demo/index.html
+
+## demo
+```
+$ npm run start
+```
+Open http://localhost:9006/demo/
---
+++ demo/src/index.js
@@ -0,0 +1,22 @@
+import {convertToByteArray, convertToDataURI, writePngDpi} from '../../src/'
+import {DpiAwareImage} from 'dpi-aware-image'
+
+const loadImage = async srcUrl => {
+  const res = await fetch(srcUrl, {mode: 'cors'})
+  const arrayBuffer = await res.arrayBuffer()
+  if (!arrayBuffer) return
+  // 遠回しだが、base64 stringを渡す実験
+  const base64DataURI = convertToDataURI(new Uint8Array(arrayBuffer))
+  const orgByteArray = convertToByteArray(base64DataURI)
+  const dpr = window.devicePixelRatio
+  const genByteArray = writePngDpi(orgByteArray, dpr)
+  showImage(convertToDataURI(genByteArray))
+}
+
+const showImage = dataUrl => {
+  const img = document.querySelector('#sample-image')
+  img.setAttribute('src', dataUrl)
+}
+
+window.loadImage = loadImage
+window.customElements.define(DpiAwareImage.is, DpiAwareImage)
